d709b240724989d411a96a434b8fcbed029e47e3
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-13 11:27:13 +0600

    component based loading implemented

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,11 +1,22 @@
 import Landing from "@/src/components/modules/home/Landing";
 import RecentPosts from "@/src/components/modules/home/RecentPosts";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-    <Landing />
-    <RecentPosts />
+      <Landing />
+      {/* Sup{" "} */}
+      <Suspense
+        fallback={
+          <h2 className="animate-pulse text-2xl text-center font-bold my-5">
+            Loading ...
+          </h2>
+        }
+      >
+        {/* <PostFeed /> */}
+        <RecentPosts />
+      </Suspense>
     </>
   );
 }
