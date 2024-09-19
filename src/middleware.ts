dfa6897dbc50e211c6bcfacd8d48b686d7446162
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-19 17:20:13 +0600

    middlewares updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,43 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+
+const AuthRoutes = ["/login", "/register"];
+const roleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
+type Role = keyof typeof roleBasedRoutes;
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  // const user = {
-  //   name: "habib",
-  //   token: "fljds fasdf",
-  //   role: "user",
-  // };
-  const user = undefined;
-  if (user?.name) {
-    return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const { pathname } = request.nextUrl;
+  // console.log(pathname);
+  const user = {
+    name: "habib",
+    token: "fljds fasdf",
+    role: "ADMIN",
+  };
+  // const user = undefined;
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
+
+  if (user?.role && roleBasedRoutes[user?.role as Role]) {
+    const routes = roleBasedRoutes[user?.role as Role];
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/admin"],
+  matcher: ["/profile", "/admin", "/login", "/register"],
 };
