718436cb0672cd59e3f32d797b21f04b86b380f7
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-21 13:27:33 +0600

    fx input updated

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -1,16 +1,22 @@
+import FXForm from "@/src/components/form/FXForm";
+import FXInput from "@/src/components/form/FXInput";
 import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
 import React from "react";
 
 const LoginPage = () => {
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="min-h-[calc(100vh-120px)] flex items-center justify-center">
       <div>
         <h2 className="my-4 text-xl font-bold">This is login page</h2>
         <div className="w-80 flex flex-col gap-4">
-          <Input type="email" label="Email"></Input>
-          <Input type="password" label="Password"></Input>
-          <Button> Login</Button>
+          <FXForm onSubmit={onSubmit}>
+            <FXInput type="email" label="Email" name="email"></FXInput>
+            <FXInput type="password" label="Password" name="password"></FXInput>
+            <Button type="submit"> Login</Button>
+          </FXForm>
         </div>
       </div>
     </div>
--- src/components/form/FXInput.tsx
+++ src/components/form/FXInput.tsx
@@ -1,17 +1,35 @@
 import { Input } from "@nextui-org/input";
+import { useFormContext } from "react-hook-form";
 
 interface IProps {
-  variant: "bordered" | "flat" | "faded" | "underlined";
-  size: "md" | "sm" | "lg";
-  required: boolean;
+  variant?: "bordered" | "flat" | "faded" | "underlined";
+  size?: "md" | "sm" | "lg";
+  required?: boolean;
+  type?: string;
+  label: string;
+  name: string;
 }
 
 const FXInput = ({
   variant = "bordered",
   size = "md",
   required = false,
+  type = "text",
+  label,
+  name,
 }: IProps) => {
-  return <Input variant={variant} size={size} required={required}></Input>;
+  const { register } = useFormContext();
+  return (
+    <Input
+      {...register}
+      variant={variant}
+      size={size}
+      required={required}
+      type={type}
+      label={label}
+      name={name}
+    ></Input>
+  );
 };
 
 export default FXInput;
