4a376084d054d854906c2a84c2d28f8f424dedf3
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-24 10:19:28 +0600

    create post basic input created

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import FXInput from "@/src/components/form/FXInput";
+import { Button } from "@nextui-org/button";
 import React from "react";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 
 const page = () => {
+  const methods = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const { control, handleSubmit } = methods;
+  const {} = useFieldArray({
+    control,
+    name: "questions",
+  });
   return (
     <div>
       <h2>This is create post for user</h2>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title"></FXInput>
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
     </div>
   );
 };
